541aaf51c66e1a092a3b1ceeacf29ec20ba9bd5e
Evenity <benjamin.miller@esrf.fr>
2017-07-04 13:22:55 +0200

    End of testing

--- py_psa/MatrixDefinition.py
+++ py_psa/MatrixDefinition.py
@@ -25,7 +25,7 @@ def MatMosMono(ThetaB, c):                                             # For a m
 def MatFlatMirr(IncAng, Sigma, Lambda, Delta):                      # For the plane mirror
     return exp(-(4*pi*np.sin(IncAng)*Sigma/Lambda)**2)*np.array([[1,0,0],[Delta,1,0],[0,0,1]])
 
-def MatMirr(IncAng, Sigma, Lambda, Delta, Fm):                      # For the bent and toroidal mirrors
+def MatMirr(IncAng, Sigma, Lambda, Delta, Fm, S):                      # For the bent and toroidal mirrors
     return exp(-(4*pi*np.sin(IncAng)*Sigma/Lambda)**2)*np.array([[1,0,0],[(1+S*Fm*Delta)/Fm,1,0],[0,0,1]])
 
 def MatParaLens(F):                                                 # For the compound refractive lenses with parabolic holes
@@ -35,72 +35,64 @@ def MatCircLens(Coef, F):                                           # For the co
     return np.array([[1,0,0],[Coef/F,1,0],[0,0,1]])
 
 def MatMulti(Fmu):                                                  # For the multilayer
-    return np.array([[1,0,0],[exp(1)/Fmu,1,0],[0,0,1]])
+    return np.array([[1,0,0],[1/Fmu,1,0],[0,0,1]])
 
 
 #Definition of the beam source
 
-def SourceX(x, SigmaSourceX, xp, SigmaSourceXp):
+def SourceX(x, xp, SigmaSourceX=1e-6, SigmaSourceXp=2e-6):
     return exp(-( (x/SigmaSourceX)**2 + (xp/SigmaSourceXp)**2) / 2 )
-def SourceY(y, SigmaSourceY, yp, GammaSource, SigmaSourceYp):
+def SourceY(y, yp, SigmaSourceY=1e-6, SigmaSourceYp=2e-6, GammaSource=5e-5):
     return exp( -( (y/SigmaSourceY)**2 + ((yp-GammaSource*y)/SigmaSourceYp)**2)/2 )
-def SourceLambda(DLambda, Lambda, SigmaSourceLambda):
-    return exp(-(DLambda/Lambda)**2/2/SigmaSourceLambda**2)
+def SourceLambda(DeltaLambda, SigmaSourceLambda=1e-6):
+    return exp(-(DeltaLambda)**2/2/SigmaSourceLambda**2)
 
 #Definition of the acceptance windows of the optical parts
 
 def cotan(x):
     return 1/np.tan(x)
 
-    #Slit acceptance
-def AccSlit(y, Aperture):
-    return sqrt(6/pi) / sqrt(6*log(2)/pi) * exp( -(y/Aperture)**2/2*12) #todo check sqrt(6*log(2)/pi)
+def AccSlit(y, Aperture):                                                           #Slit acceptance
+    return sqrt(6/pi) / sqrt(6*log(2)/pi) * exp( -(y/Aperture)**2/2*12)
 
-    #Pinhole acceptance
-def AccPinh(y, Diameter):
+def AccPinh(y, Diameter):                                                            #Pinhole acceptance
     return sqrt(8/pi) * exp ( -(y/Diameter)**2/2*16 )
 
-    #Plane monochromator angle acceptance
-def AccMonoPlanAng(RMono, RInt, Wd, yp, DeltaLambda, Lambda, ThetaB):
-    return RMono*RInt*sqrt(6/pi)/Wd * exp( -(yp-DeltaLambda/Lambda*np.tan(ThetaB))**2 / (2*Wd**2/12))
+def AccMonoPlanAng(RMono, RInt, Wd, yp, DeltaLambda, ThetaB):                #Plane monochromator angle acceptance
+    return RMono*RInt*sqrt(6/pi)/Wd * exp( -(yp-DeltaLambda*np.tan(ThetaB))**2 / (2*Wd**2/12))
 
-    #Plane monochromator wave acceptance
-def AccMonoPlanWav(DeltaLambda, Lambda, SigmaYp, ThetaB, Wd):
-    return sqrt(6/pi) * exp( - (DeltaLambda/Lambda)**2 / (2*(SigmaYp**2+Wd**2/12)*cotan(ThetaB)**2) )
+def AccMonoPlanWav(DeltaLambda, SigmaYp, ThetaB, Wd):                        #Plane monochromator wave acceptance
+    return sqrt(6/pi) * exp( - (DeltaLambda)**2 / (2*(SigmaYp**2+Wd**2/12)*cotan(ThetaB)**2) )
 
-    #Mosaic monochromator angular acceptance
-def AccMonoMosaAng(RInt, eta, yp, DeltaLambda, Lambda, ThetaB):
-    return RInt*sqrt(6/pi)/eta * exp(- (yp-DeltaLambda/Lambda*np.tan(ThetaB))**2 / eta**2 /2 ) # todo : check /2 and RInt
+def AccMonoMosaAng(RInt, eta, yp, DeltaLambda, ThetaB):                       #Mosaic monochromator angular acceptance
+    return RInt*sqrt(6/pi)/eta * exp(- (yp-DeltaLambda*np.tan(ThetaB))**2 / eta**2 /2 )
 
-    #Mosaic monochromator wave acceptance
-def AccMonoMosaWav(DeltaLambda, Lambda, SigmaYp, eta, ThetaB):
-    return sqrt(6/pi) * exp( - (DeltaLambda/Lambda)**2 / 2 /((SigmaYp**2+eta**2)*cotan(ThetaB))) # todo : check Sigma Yp
+def AccMonoMosaWav(DeltaLambda, SigmaYp, eta, ThetaB):                        #Mosaic monochromator wave acceptance
+    return sqrt(6/pi) * exp( - (DeltaLambda)**2 / 2 /((SigmaYp**2+eta**2)*cotan(ThetaB)**2))
 
-    #Curved monochromator angle acceptance
-def AccMonoCurvAng(RMono, RInt, Wd,yp, y, r, ThetaB, Alpha, DeltaLambda, Lambda ):
-    return RMono*RInt*sqrt(6/pi)/Wd * exp( - (yp-y/r/np.sin(ThetaB+Alpha)-DeltaLambda/Lambda*np.tan(ThetaB))**2/(2*Wd**2/12)) # todo check yp-y
+def AccMonoCurvAng(RMono, RInt, Wd,yp, y, r, ThetaB, Alpha, DeltaLambda ):    #Curved monochromator angle acceptance
+    return RMono*RInt*sqrt(6/pi)/Wd * exp( - (yp-y/r/np.sin(ThetaB+Alpha)-DeltaLambda*np.tan(ThetaB))**2/(2*Wd**2/12))
 
-    #Curved monochromator wave acceptance
-def AccMonoCurvWav(DeltaLambda,Lambda, ThetaB, SigmaSource, DistanceFromSource, Wd):
-    return sqrt(6/pi) * exp( -(DeltaLambda/Lambda)**2 / (2*cotan(ThetaB)**2*((SigmaSource/DistanceFromSource)**2+Wd**2/12))   ) #todo check too
+def AccMonoCurvWav(DeltaLambda, ThetaB, SigmaSource, DistanceFromSource, Wd):  #Curved monochromator wave acceptance
+    return sqrt(6/pi) * exp( -(DeltaLambda)**2 / (2*cotan(ThetaB)**2*((SigmaSource/DistanceFromSource)**2+Wd**2/12))   )
 
-    #Compound refractive lense with parabolic holes acceptance
-def AccLensPara(x, Radius, Distance, Sigma):
+def AccLensPara(x, Radius, Distance, Sigma):                                          #Compound refractive lense with parabolic holes acceptance
     return exp( -(x**2+Radius*Distance)/2/Sigma**2)
 
-    #Compound refractive lense with circular holes acceptance
-def AccLensCirc(x, Sigma, FWHM, Radius, Distance):
-    return exp( -x**2/2/Sigma**2 -x**2*FWHM**2/8/Radius**2/Sigma**2 -x**2*FWHM**4/16/Sigma**2/Radius**4 -Radius*Distance/2/Sigma**2  ) #todo why FWHM
+def AccLensCirc(x, Sigma, FWHM, Radius, Distance):                                    #Compound refractive lense with circular holes acceptance
+    return exp( -x**2/2/Sigma**2 -x**2*FWHM**2/8/Radius**2/Sigma**2 -x**2*FWHM**4/16/Sigma**2/Radius**4 -Radius*Distance/2/Sigma**2  )
 
-    #Multilayer angle acceptance
-def AccMultAng(Rml, yp, y, Rowland, ThetaML, DeltaLambda, Lambda, Ws):
-    return Rml*8/3*sqrt(log(2)/pi) * exp( -(-yp-y/Rowland/np.sin(ThetaML)-DeltaLambda/Lambda*np.tan(ThetaML))**2/2/Ws**2 )
+def AccMultAng(Rml, yp, y, Rowland, ThetaML, DeltaLambda, Ws):                #Multilayer angle acceptance
+    return Rml*8/3*sqrt(log(2)/pi) * exp( -(-yp-y/Rowland/np.sin(ThetaML)-DeltaLambda*np.tan(ThetaML))**2*8*log(2)/2/Ws**2 )
 
-    #Multilayer wave acceptance
-def AccMultWav(DeltaLambda,Lambda, SigmaSource, DistanceFromSource, Ws, ThetaML):
-    return sqrt(6/pi) * exp( -(DeltaLambda/Lambda)**2/2/((SigmaSource/DistanceFromSource)**2+Ws**2/8/log(2))/cotan(ThetaML)**2)
+def AccMultWav(DeltaLambda, SigmaSource, DistanceFromSource, Ws, ThetaML):     #Multilayer wave acceptance
+    return sqrt(6/pi) * exp( -(DeltaLambda)**2/2/((SigmaSource/DistanceFromSource)**2+Ws**2/8/log(2))/cotan(ThetaML)**2)
+
 
 #Testing the functions
+
+
+    #Testing the matrices
 
 def testMatFlatMono():
     print(">> MatFlatMono in test")
@@ -146,3 +138,233 @@ def testMatFlatMirr():
     result = MatFlatMirr(IncAng, Sigma, Lambda, Delta)
     print("Mistakes ?", nut.assert_array_almost_equal(Mat, result, decimal=5))
 print(testMatFlatMirr())
+
+def testMatMirr():
+    print(">> MatMirr in test")
+    Sigma = 0.02
+    IncAng = 30
+    Lambda = 1
+    Delta = 5
+    Fm = 10
+    S =0.1
+    Mat = np.array([[0.9402, 0, 0], [0.56412, 0.9402, 0], [0, 0, 0.9402]])
+    print(Mat.shape)
+    result = MatMirr(IncAng, Sigma, Lambda, Delta, Fm, S)
+    print("Mistakes ?", nut.assert_array_almost_equal(Mat, result, decimal=5))
+print(testMatMirr())
+
+def testMatParaLens():
+    print(">> MatParaLens in test")
+    F=12.3
+    Mat = np.array([[1, 0, 0], [0.0813008, 1, 0], [0, 0, 1]])
+    print(Mat.shape)
+    result = MatParaLens(F)
+    print("Mistakes ?", nut.assert_array_almost_equal(Mat, result, decimal=5))
+print(testMatParaLens())
+
+def testMatCircLens():
+    print(">> MatCircLens in test")
+    F = 12.3
+    Coef = 0.4
+    Mat = np.array([[1, 0, 0], [0.0325203, 1, 0], [0, 0, 1]])
+    print(Mat.shape)
+    result = MatCircLens(Coef, F)
+    print("Mistakes ?", nut.assert_array_almost_equal(Mat, result, decimal=5))
+print(testMatCircLens())
+
+def testMatMulti():
+    print(">> MatMulti in test")
+    Fmu = 15.1
+    Mat = np.array([[1, 0, 0], [0.0662252, 1, 0], [0, 0, 1]])
+    print(Mat.shape)
+    result = MatMulti(Fmu)
+    print("Mistakes ?", nut.assert_array_almost_equal(Mat, result, decimal=5))
+print(testMatMulti())
+
+    #Testing the source
+
+def testSourceX():
+    print(">> SourceX in test")
+    x = 0.5
+    xp = 2
+    Source = 0.605773
+    result = SourceX(x, xp, SigmaSourceX=10, SigmaSourceXp=2)
+    print(Source, result)
+    print("Mistakes ?", nut.assert_almost_equal(Source, result, decimal=5))
+print(testSourceX())
+
+def testSourceY():
+    print(">> SourceY in test")
+    y = 0.5
+    yp = 2
+    Source = 0.753897
+    result = SourceY(y, yp, SigmaSourceY=10, SigmaSourceYp=2, GammaSource=1)
+    print(Source, result)
+    print("Mistakes ?", nut.assert_almost_equal(Source, result, decimal=5))
+print(testSourceY())
+
+def testSourceLambda():
+    print(">> SourceLambda in test")
+    DeltaLambda = 4
+    Source = 0.278037
+    result = SourceLambda(DeltaLambda, SigmaSourceLambda=2.5)
+    print(Source, result)
+    print("Mistakes ?", nut.assert_almost_equal(Source, result, decimal=5))
+print(testSourceLambda())
+
+    #Testing the acceptances
+
+def testAccSlit():
+    print(">> AccSlit in test")
+    y = 2
+    Aperture = 75
+    Acceptance = 1.19601
+    result = AccSlit(y, Aperture)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccSlit())
+
+def testAccPinh():
+    print(">> AccPinh in test")
+    y = 2
+    Diameter = 75
+    Acceptance = 1.58672
+    result = AccPinh(y, Diameter)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccPinh())
+
+def testAccMonoPlanAng():
+    print(">> AccMonoPlanAng in test")
+    RMono = 0.91
+    RInt = 0.2
+    Wd = 103.2
+    yp = 0.001
+    DeltaLambda = 0.2
+    ThetaB = 33
+    Acceptance = 0.00214474
+    result = AccMonoPlanAng(RMono, RInt, Wd, yp, DeltaLambda, ThetaB)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccMonoPlanAng())
+
+def testAccMonoPlanWav():
+    print(">> AccMonoPlanWav in test")
+    Wd = 103.2
+    SigmaYp = 0.001
+    DeltaLambda = 0.2
+    ThetaB = 33
+    Acceptance = 1.21616
+    result = AccMonoPlanWav(DeltaLambda, SigmaYp, ThetaB, Wd)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccMonoPlanWav())
+
+def testAccMonoMosaAng():
+    print(">> AccMonoMosaAng in test")
+    RMono = 0.91
+    RInt = 0.2
+    eta = 103.2
+    yp = 0.001
+    DeltaLambda = 0.2
+    ThetaB = 33
+    Acceptance = 0.00264987
+    result = AccMonoMosaAng(RInt, eta, yp, DeltaLambda, ThetaB)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccMonoMosaAng())
+
+def testAccMonoMosaWav():
+    print(">> AccMonoMosaWav in test")
+    eta = 103.2
+    SigmaYp = 0.001
+    DeltaLambda = 0.2
+    ThetaB = 33
+    Acceptance = 1.36733
+    result = AccMonoMosaWav(DeltaLambda, SigmaYp, eta, ThetaB)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccMonoMosaWav())
+
+def testAccMonoCurvAng():
+    print(">> AccMonoCurvAng in test")
+    RMono = 0.91
+    RInt = 0.2
+    Wd = 103.2
+    yp = 0.001
+    y = 8
+    r = 9
+    Alpha = 0.5
+    DeltaLambda = 0.2
+    ThetaB = 33
+    Acceptance = 0.00218093
+    result = AccMonoCurvAng(RMono, RInt, Wd, yp, y, r, ThetaB, Alpha, DeltaLambda)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccMonoCurvAng())
+
+def testAccMonoCurvWav():
+    print(">> AccMonoCurvWav in test")
+    Wd = 103.2
+    SigmaSource = 5.5
+    DistanceFromSource = 0.001
+    DeltaLambda = 0.2
+    ThetaB = 33
+    Acceptance = 1.38197
+    result = AccMonoCurvWav(DeltaLambda, ThetaB, SigmaSource, DistanceFromSource, Wd)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccMonoCurvWav())
+
+def testAccLensPara():
+    print(">> AccLensPara in test")
+    x = 2
+    Radius = 75
+    Distance = 22.2
+    Sigma = 33.3
+    Acceptance = 0.471162
+    result = AccLensPara(x, Radius, Distance, Sigma)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccLensPara())
+
+def testAccLensCirc():
+    print(">> AccLensCirc in test")
+    x = 2
+    Radius = 75
+    Distance = 22.2
+    FWHM = 43.3
+    Sigma = 33.3
+    Acceptance = 0.471079
+    result = AccLensCirc(x, Sigma, FWHM, Radius, Distance)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccLensCirc())
+
+def testAccMultAng():
+    print(">> AccMultAng in test")
+    Rml = 200
+    yp = 0.03
+    y = 22.2
+    Rowland = 443.3
+    ThetaML = 1
+    DeltaLambda = 1
+    Ws = 0.7
+    Acceptance = 0.0000541429
+    result = AccMultAng(Rml, yp, y, Rowland, ThetaML, DeltaLambda, Ws)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccMultAng())
+
+def testAccMultWav():
+    print(">> AccMultWav in test")
+    ThetaML = 1
+    DeltaLambda = 1
+    Ws = 0.7
+    DistanceFromSource = 22
+    SigmaSource = 45
+    Acceptance = 1.04044
+    result = AccMultWav(DeltaLambda, SigmaSource, DistanceFromSource, Ws, ThetaML)
+    print(Acceptance, result)
+    print("Mistakes ?", nut.assert_almost_equal(Acceptance, result, decimal=5))
+print(testAccMultWav())
